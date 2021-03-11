e46e0d49005bf6e3b2a9ab74dbf1b583d0214edc
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-03-11 15:41:01 +0400

    change label position if input is not empty

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import "./css/style.css";
 import "./css/customStyles.css";
+console.log(process.env.WEATHER_KEY);
+
+const label = document.querySelector('.form-input-label');
+const search = document.getElementById("search-input");
+
+console.log(search);
+search.addEventListener('input', (event) => {
+  label.classList.add('label-over-input')
+  if (search.value.length>0){
+
+    label.classList.add('label-over-input')
+    label.classList.remove('label-inside-input')
+  }else{
+    label.classList.add('label-inside-input')
+    label.classList.remove('label-over-input')
+  }
+
+});
 
 
-console.log(process.env.WEATHER_KEY)
+const searchButton = document.getElementById("search-button");
+
+
+searchButton.addEventListener
