24d98739c191b939f05f78c848146f5bb1a1c7ae
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-03-14 13:51:58 +0400

    make fahrenheit to show only first two digits

--- src/utils.js
+++ src/utils.js
@@ -12,34 +12,48 @@ export const getDayOfWeek = () => {
   return weekday[d.getDay()];
 };
 
-
-export const renderData = (type,weather,temp,icon,description,min,max,sunrise,sunset)=>{
-  temp.innerText =type==='°C' ? weather.temp+type : toggleTemperature(weather.temp) + type;
+export const renderData = (
+  type,
+  weather,
+  temp,
+  icon,
+  description,
+  min,
+  max,
+  sunrise,
+  sunset
+) => {
+  temp.innerText =
+    type === "°C"
+      ? weather.temp + type
+      : toggleTemperature(weather.temp) + type;
   icon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
-  
-  description.innerText = weather.description;
-  min.innerText = type==='°C' ? weather.temp_min+type : toggleTemperature(weather.temp_min)  + type;
-  max.innerText = type==='°C' ? weather.temp_max + type : toggleTemperature(weather.temp_max)  + type;
 
-  sunrise.innerText = new Date(weather.sunrise * 1000).toLocaleTimeString(
-    [],
-    {
-      hour: "2-digit",
-      minute: "2-digit",
-    }
-  );
+  description.innerText = weather.description;
+  min.innerText =
+    type === "°C"
+      ? weather.temp_min + type
+      : toggleTemperature(weather.temp_min) + type;
+  max.innerText =
+    type === "°C"
+      ? weather.temp_max + type
+      : toggleTemperature(weather.temp_max) + type;
+
+  sunrise.innerText = new Date(weather.sunrise * 1000).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
   sunset.innerText = new Date(weather.sunset * 1000).toLocaleTimeString([], {
     hour: "2-digit",
     minute: "2-digit",
   });
-}
+};
 
-export const toggleTemperature = temp=>{
+export const toggleTemperature = (temp) => {
+  temp = (temp * 9) / 5 + 32;
 
-  temp= temp * 9 / 5 + 32;
-  
-  // temp = (temp - 32) * 5 / 9;
+  temp = temp.toFixed(2);
 
-  return temp
-}
+  return temp;
+};
